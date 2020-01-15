b74c2a59e4033c323014ee4dd7e90dfa8d3e6959
Gabriel Bruno Meinchein <gabrielbruno.me@gmail.com>
2020-01-14 23:40:47 -0300

    Recuperando pagina 1 anuncios webmotors

--- buscarros/spiders/Webmotors.py
+++ buscarros/spiders/Webmotors.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 from buscarros.items import BuscarrosItem
 from scrapy.http.request import Request
@@ -6,7 +7,7 @@ from scrapy.http.request import Request
 class WebmotorsSpider(scrapy.Spider):
     name = 'Webmotors'
     allowed_domains = ['webmotors.com.br']
-    start_urls = ['https://www.webmotors.com.br/carros/sc/fiat?estadocidade=Santa%20Catarina&tipoveiculo=carros&marca1=FIAT']
+    start_urls = ['https://www.webmotors.com.br/api/search/car?url=https://www.webmotors.com.br/carros%2Fsc%2Ffiat%3Festadocidade%3DSanta%2520Catarina&actualPage=1&displayPerPage=24&order=1&showMenu=true&showCount=true&showBreadCrumb=true&testAB=false&returnUrl=false']#['https://www.webmotors.com.br/carros/sc/fiat?estadocidade=Santa%20Catarina&tipoveiculo=carros&marca1=FIAT']
 
     def start_requests(self):
         headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
@@ -14,21 +15,18 @@ class WebmotorsSpider(scrapy.Spider):
             yield Request(url, headers=headers)
 
     def parse(self, response):
-        titles = response.css('h2.CardVehicle__details__header__title::text').extract()
-        hrefs = response.css('a.CardVehicle__link::attr(href)').extract()
-        # prices = response.css('p.OLXad-list-price::text').extract()
-        # images = response.css('img.image.lazy::attr(data-original)').extract()
-        # adresses = response.css('p.text.detail-region::text').extract()
+        jsonresponse = json.loads(response.body)
+        
+        i = 0
+        for SearchResult in jsonresponse["SearchResults"]:
+            
+            new_item = BuscarrosItem()          
 
-        for item in zip(titles, hrefs):
-
-            new_item = BuscarrosItem()
-
-            new_item['title'] = item[0]
-            new_item['href'] = item[1]
-            new_item['price'] = item[2]
-            # new_item['image'] = item[3]
-            # new_item['adress'] = item[4]
+            new_item['title'] = SearchResult["Specification"]["Title"]
+            new_item['href'] = SearchResult["Specification"]["Title"]
+            new_item['price'] = str(SearchResult["Prices"]["Price"])
+            new_item['image'] = "https://image.webmotors.com.br/_fotos/anunciousados/gigante/" + SearchResult["Media"]["Photos"][0]["PhotoPath"]
+            new_item['adress'] = SearchResult["Seller"]["City"]
 
             yield new_item
         
